6b2329d1379692d47aedc21f49cbe238d1cb6a46
FatimahHabib84 <fatimahhabib84@gmail.com>
2024-05-21 18:30:33 +0300

    lap-1-JS

---
+++ main.js
@@ -0,0 +1,35 @@
+let height = 8
+let width = 6
+console.log(`Area is ${width*height}`)
+
+let C = 50
+let F = (C*1.8)+32
+console.log(`Tempreture ${C} °C is ${F} °F in fehrenhit`)
+
+let hour = 3
+let seconds = 60
+console.log(`Number of seconds on ${3} hours is = ${3*60*60} seconds `)
+
+let thingOne="My name is"
+let thingTwo=" Fatimah"
+console.log(thingOne.concat(thingTwo))
+
+let bill = 10.25 + 3.99 + 7.15
+let tip = bill*0.15
+let total = bill + tip
+console.log(total)
+var madLib  ="The Intro to JavaScript course is __________. James and Julia are so __________. I cannot wait to work through the rest of this __________ content!"
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+var madLib  = madLib .replace('__________',adjective1)
+madLib  = madLib .replace('__________',adjective2)
+madLib  = madLib .replace('__________',adjective3)
+console.log(madLib)
+
+
+var firstName = "Fatimah"
+var interest = "Qute"
+var hobby = "draw"
+var awesomeMessage = `Hi, my name is ${firstName}. I love ${interest}. In my spare time, I like to ${hobby}.`
+console.log(awesomeMessage)
